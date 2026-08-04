0d12037b9d2f5363d0647e3f4852f5122ede8f72
pikipik5758 <pikipik5758@gmail.com>
2026-08-04 10:02:18 +0700

    feat: add postgres database connection

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 const express = require('express');
+const{ Pool } = require('pg');
 const app = express();
-const PORT = process.env.PORT || 3000;
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000; 
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL || 'postgresql://username:password@localhost:5432/devops_db',
+});
 
 // Endpoint 1: Pesan Selamat Datang
 app.get('/', (req, res) => {
   res.json({ 
-    message: "SUDAH DEPLOY! Selamat datang di microservice DevOps.",
+    message: "Multi-Container DevOps Microservice is running!",
     timestamp: new Date()
   });
 });
 
 // Endpoint 2: Health Check (Penting untuk DevOps kelak!)
 app.get('/health', (req, res) => {
-  res.status(200).json({ status: "UP", service: "devops-microservice" });
+  res.json({ status: "UP", timestamp: new Date() });
+});
+
+
+// tes koneksi ke database
+app.get('/db-check', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json({ 
+      message: "Database connection successful!",
+      db_time: result.rows[0].now
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Database connection failed!",
+      error: error.message
+    });
+  }
 });
 
 app.listen(PORT, () => {
